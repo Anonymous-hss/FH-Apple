4aae39483efafeed5a450a351b8b1cd26bc84568
Anonymous-hss <harshalsawatkar24@gmail.com>
2024-09-04 12:02:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlight from "./components/Highlights";
+
+function App() {
+  return (
+    <main className="bg-black">
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Highlight></Highlight>
+    </main>
+  );
+}
+
+export default App;
